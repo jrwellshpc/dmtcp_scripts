d65b7d3fdd7df879c7450b4a62dcd07d7e6c4dc1
Jason Wells <wellsj@wellsj.com>
2022-02-03 15:29:09 -0500

    Adding GPU AIML example

---
+++ gpu/tf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an \"AS IS\" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import tensorflow as tf
+import time
+import sys
+
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if logs.get('accuracy') is not None and logs.get('accuracy') >= 0.95:
+    #if(logs.get('accuracy')>0.95):
+      print("\nReached 95% accuracy so cancelling training!")
+      self.model.stop_training = True
+
+file_path='python_output'
+sys.stdout=open(file_path, "a")
+print('starting\n')
+callbacks = myCallback()
+mnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+training_images=training_images/255.0
+test_images=test_images/255.0
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation=tf.nn.relu),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(training_images, training_labels, epochs=500, callbacks=[callbacks])
+print("done\n")
+
