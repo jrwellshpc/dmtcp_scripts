c7b1b2bfca2f0e9e9de691d3cdfb726be670ea96
Jason Wells <98784673+jrwellshpc@users.noreply.github.com>
2022-02-09 08:31:02 -0500

    Update tf.py

--- gpu/tf.py
+++ gpu/tf.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
- 
+# Credit for this code goes to https://github.com/lmoroney/tfbook/blob/master/chapter2/fashion-mnist-withcallbacks.py. 
 # Licensed under the Apache License, Version 2.0 (the "License")
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
